036a9bcaccfbf5996d21fd9618e9b17f68b3e644
Pedro Lucas <pedrolucascaniato@gmail.com>
2024-09-04 17:24:46 -0300

    Registro Atualizado

---
+++ crud/firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCk_HPul7TIYBZ8eHfmg3yaQnQ7f2BDzVw",
+    authDomain: "controledegastos-78359.firebaseapp.com",
+    projectId: "controledegastos-78359",
+    storageBucket: "controledegastos-78359.appspot.com",
+    messagingSenderId: "984831189290",
+    appId: "1:984831189290:web:0c646863a0d62b5378fe53"
+  };
+
+  firebase.initializeApp(firebaseConfig);
